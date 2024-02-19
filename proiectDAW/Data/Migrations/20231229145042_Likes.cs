13f513f25c63aeb1b0f60db1ea518464e156b4b0
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2024-02-19 11:34:38 +0200

    Added the final project

---
+++ proiectDAW/Data/Migrations/20231229145042_Likes.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proiectDAW.Data.Migrations
+{
+    public partial class Likes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLikesBookmarks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookmarkId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLikesBookmarks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserLikesBookmarks_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLikesBookmarks_Bookmarks_BookmarkId",
+                        column: x => x.BookmarkId,
+                        principalTable: "Bookmarks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesBookmarks_BookmarkId",
+                table: "UserLikesBookmarks",
+                column: "BookmarkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesBookmarks_UserId",
+                table: "UserLikesBookmarks",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserLikesBookmarks");
+        }
+    }
+}
---
+++ proiectDAW/Data/Migrations/20231229162814_test222222.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proiectDAW.Data.Migrations
+{
+    public partial class test222222 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserLikesBookmarks",
+                table: "UserLikesBookmarks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserLikesBookmarks",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookmarkId",
+                table: "UserLikesBookmarks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserLikesBookmarks",
+                table: "UserLikesBookmarks",
+                columns: new[] { "Id", "BookmarkId", "UserId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserLikesBookmarks",
+                table: "UserLikesBookmarks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserLikesBookmarks",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookmarkId",
+                table: "UserLikesBookmarks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserLikesBookmarks",
+                table: "UserLikesBookmarks",
+                column: "Id");
+        }
+    }
+}
